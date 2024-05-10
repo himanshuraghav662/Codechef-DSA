03ad20f2ae7ba520bab257b6bf55ce4f8200ced5
himanshuraghav662 <108710259+himanshuraghav662@users.noreply.github.com>
2024-05-10 21:48:30 +0530

    Update running comparison .java

--- running comparison .java
+++ running comparison .java
@@ -1,1 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
 
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            int[] a = new int[n];
+            int[] b = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = scanner.nextInt();
+            }
+
+            for (int i = 0; i < n; i++) {
+                b[i] = scanner.nextInt();
+            }
+            int d=0;
+        for(int i=0;i<n;i++){
+           if(a[i]>b[i] && b[i]*2>=a[i]){
+               d++;
+           }
+           if(b[i]>a[i] && a[i]*2>=b[i]){
+               d++;
+           }
+           if(a[i]==b[i])
+           d++;
+        }
+        System.out.println(d);
+        }
+        // Your code goes here
+        
+    }
+}
